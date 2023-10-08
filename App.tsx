3637c101e12241e96bfd7f9abd3e9ceb83d6643f
vaisakh.cp@ima360.com <121595062+vaisakhChandranP@users.noreply.github.com>
2023-10-09 02:48:41 +0530

    intermediate

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { DataProvider } from './src/hooks';
+import AppNavigation from './src/navigation/App';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
+export default function App() {
+  return (
+    <DataProvider>
+      <AppNavigation />
+    </DataProvider>
+  );
+}
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "healthcarenaukriexpo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "healthcarenaukriexpo19a85b5a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
